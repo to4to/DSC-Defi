8d54278f97f18455bcea26ad7da437958714a6e6
to4to <justjustifying@outlook.com>
2024-06-11 06:46:06 +0530

    chore: Update Solidity version to 0.8.20 in DSCEngine.sol

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
+
+//import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggeratorV3Interface.sol";
+
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+library OracleLin {
+    
+error OracleLib_StalePrice();
+
+    uint private constant TIOMEOUT=3 hours;
+
+
+    function staleCheckLatestRoundData(AggregatorV3Interface chainlinkFeed) public view  returns (uint80 roundId,) {
+        
+    }
+
+
+
+}
