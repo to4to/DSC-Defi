d8a807f2ceb0091581889e714ff8eb319b3850ae
to4to <justjustifying@outlook.com>
2024-10-20 19:13:52 +0530

    feat: Add error for mismatched token and price feed addresses; update constructor logic

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -7,6 +7,7 @@ contract DSCEngine {
 
     error DSCEngine_AmountShouldBeMoreThanZer();
     error DSCEngine_CheckAddressOFToken();
+    error DSCEngine_TokenAddressAndPriceFeedAddressDoNotMatch();
 
     using OracleLib for AggeratorV3Interface;
 
@@ -31,9 +32,15 @@ modifier isAllowedToke(address token){
 constructor(address[] memory tokenAddress,address[] memory priceFeedAddress,address dscAddress){
 
 
+    if (tokenAddress.length!=priceFeedAddress.length){
+        revert DSCEngine_TokenAddressAndPriceFeedAddressDoNotMatch();
+    }
 
-
-
+    for (uint256 i = 0; i < tokenAddresses.length; i++) {
+               s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+               s_collateralTokens.push(tokenAddresses[i]);
+           }
+           i_dsc = DecentralizedStableCoin(dscAddress);
 
 }
 
